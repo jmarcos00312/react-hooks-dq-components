eae36bb375003065c5337c4047ba7d8a73d751a5
Jeremiah <jeremiahmarcos27@outlook.com>
2021-11-02 11:42:35 -0500

    completed task

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import MainContent from "./MainContent";
+import TopMenu from "./TopMenu";
+import SideMenu from "./SideMenu";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">
+    <MainContent />
+    <TopMenu />
+    <SideMenu />
+  </div>;
 }
 
 export default App;
